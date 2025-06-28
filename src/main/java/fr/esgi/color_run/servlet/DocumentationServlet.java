d75d7a8e6bbf4d8a4494f11aaa777b53c2f0be66
Furrioxx <114649898+Furrioxx@users.noreply.github.com>
2025-06-28 11:31:51 +0200

    [bugfix/documentation] add footer link + legal mentions

---
+++ src/main/java/fr/esgi/color_run/servlet/DocumentationServlet.java
@@ -0,0 +1,45 @@
+package fr.esgi.color_run.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.context.Context;
+
+import java.io.IOException;
+
+/**
+ * Servlet qui gère la documentaion de l'app "les footers etc"
+ */
+@WebServlet(name = "documentationServlet", value = {"/faq", "/about-us", "/contact", "/legal-mentions"})
+public class DocumentationServlet extends BaseWebServlet{
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Création du contexte Thymeleaf
+        Context context = new Context();
+
+        switch (request.getServletPath()) {
+            case "/faq":
+                renderTemplate(request, response, "doc/faq", context);
+                break;
+            case "/about-us":
+                renderTemplate(request, response, "doc/aboutUs", context);
+                break;
+            case "/contact":
+                renderTemplate(request, response, "doc/contact", context);
+                break;
+            case "/legal-mentions":
+                renderTemplate(request, response, "doc/legalMentions", context);
+                break;
+
+
+        }
+    }
+}
